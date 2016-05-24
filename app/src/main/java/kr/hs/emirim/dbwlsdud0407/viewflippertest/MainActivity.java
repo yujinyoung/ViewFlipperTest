0d3e9dbfe536e0011735929fb3d95247275407ac
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-05-24 20:09:27 +0900

    #2 event Programing

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/viewflippertest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/viewflippertest/MainActivity.java
@@ -2,12 +2,34 @@ package kr.hs.emirim.dbwlsdud0407.viewflippertest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  ViewFlipper viewFlip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button butPrev=(Button)findViewById(R.id.but_prev);
+        Button butNext=(Button)findViewById(R.id.but_next);
+        butPrev.setOnClickListener(this);
+        butNext.setOnClickListener(this);
+        viewFlip=(ViewFlipper)findViewById(R.id.view_flip);
+
+    }
+    @Override
+    public void onClick(View v) {
+     switch(v.getId()){
+         case R.id.but_prev:
+             viewFlip.showPrevious();
+             break;
+         case R.id.but_next:
+             viewFlip.showNext();
+             break;
+        }
     }
 }
